9d87b55b7b08fcf413e24d960f357089c6525b3a
Allen Li <darkfeline@felesatra.moe>
2017-10-21 15:44:50 -0700

    Add hvdb links

--- linkify.js
+++ linkify.js
@@ -6,8 +6,13 @@
   function linkify(query) {
     query.each(function() {
       this.innerHTML = this.innerHTML.replace(
-          /RJ([0-9]{6})(?!<\/a>|" rel=)/gi,
-        '<a href="http://www.dlsite.com/maniax/work/=/product_id/RJ$1" rel="noreferrer">RJ$1</a>');
+          /(RJ[0-9]{6})(?!<\/a>|" rel=)/gi,
+        `
+<a href="http://www.dlsite.com/maniax/work/=/product_id/$1" rel="noreferrer">$1</a>
+<small>
+[<a href="http://hvdb.me/Dashboard/WorkDetails/$1" rel="noreferrer">HVDB</a>]
+</small>
+`);
     });
   }
 
