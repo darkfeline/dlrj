3cb05c1aa744e6480a08a397a9dca2680b596b40
Allen Li <darkfeline@felesatra.moe>
2017-10-21 23:32:44 -0700

    Just use announce

--- linkify.js
+++ linkify.js
@@ -7,7 +7,7 @@
     query.each(function() {
       this.innerHTML = this.innerHTML.replace(
           /(RJ[0-9]{6})(?!<\/a>|" rel=)/gi,
-        `<a href="http://www.dlsite.com/maniax/work/=/product_id/$1" rel="noreferrer">$1</a>`);
+        `<a href="${DLSiteAnnounceURL('$1')}" rel="noreferrer">$1</a>`);
     });
   }
 
