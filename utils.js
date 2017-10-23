5768827a76bde23b101c374b2f1c15d4e4bcd434
Allen Li <darkfeline@felesatra.moe>
2017-10-22 20:17:01 -0700

    Add work specific page link

--- utils.js
+++ utils.js
@@ -14,6 +14,7 @@ function addLinkbar(code) {
   jQuery('body').prepend(`
 <div style="padding:0.5em;background-color:lightgray;font-size:large;">
 <a href="${DLSiteAnnounceURL('RJ' + code)}" rel="noreferrer">DLSite</a>
+(<a href="${DLSiteWorkURL('RJ' + code)}" rel="noreferrer">work</a>)
 <a href="${hvdbURL(code)}" rel="noreferrer">HVDB</a>
 </div>
 `);
