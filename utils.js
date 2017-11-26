a71bbe621b5ac99d5cafb677b66f9f4e85e5cf29
Allen Li <darkfeline@felesatra.moe>
2017-11-25 23:14:51 -0800

    Add links to nyaa on dlsite pages

--- utils.js
+++ utils.js
@@ -16,12 +16,26 @@ function hvdbURL(code) {
   return `https://hvdb.me/Dashboard/WorkDetails/${code}`;
 }
 
-function addLinkbar(code) {
+function addLinkbar(code, extra='') {
   jQuery('body').prepend(`
 <div style="padding:0.5em;background-color:lightgray;font-size:large;">
 <a href="${DLSiteWorkURL('RJ' + code)}" rel="noreferrer">DLSite</a>
 (<a href="${DLSiteAnnounceURL('RJ' + code)}" rel="noreferrer">announce</a>)
 <a href="${hvdbURL(code)}" rel="noreferrer">HVDB</a>
+${extra}
 </div>
 `);
+}
+
+function addLinkbarWithNyaa(code, title) {
+  addLinkbar(code, `
+<a href="https://sukebei.nyaa.si/?q=${encodeURIComponent(title)}">sukebei</a>
+`)
+}
+
+/* For DLSite */
+function getWorkTitle() {
+  var elem = jQuery('#work_name').find('a').clone();
+  elem.children().remove();
+  return elem.text().trim();
 }
