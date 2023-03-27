f7e98b03de44a8ec9d2c71018f831f6d0485de1a
Allen Li <darkfeline@felesatra.moe>
2023-03-27 01:24:36 -0700

    Handle new longer IDs

--- linkify.js
+++ linkify.js
@@ -5,7 +5,7 @@
 
   function linkifyRJ(string) {
     return string.replace(
-        /RJ([0-9]{6})(?!<\/a>|" rel=)/gi,
+        /RJ([0-9]{6-8})(?!<\/a>|" rel=)/gi,
       `<a href="${DLSiteWorkURL('RJ$1')}" rel="noreferrer">RJ$1</a>`);
   }
 
